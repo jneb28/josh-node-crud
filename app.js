9a7b5985ceae2944088c70ab60f775a9d3826464
Joshua Nebeker <10528790@my.uvu.edu>
2019-02-23 23:44:04 -0700

    revert aggregation

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const bodyParser = require('body-parser');
 const express = require('express');
-const mongodb = require('mongodb');
+//const mongodb = require('mongodb');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -19,18 +19,17 @@ app.use(playerRoutes);
 mongoose.connect(process.env.MONGODB_URI)
 .then(result => {
   console.log('Connected to Cluster!');
-
-  mongodb.test.aggregate([
-    {
-      $addFields: {
-        $winRatio: {"$multiply": [ { "$divide": ["$wins","$losses"] }, 100 ]}
-      }
-    },
-    {
-      $sort: {winRatio: 1}
-    }
-  ]);
-
   app.listen(process.env.PORT || port);  
 })
 .catch(err => console.log(err));
+
+// mongodb.test.aggregate([
+//   {
+//     $addFields: {
+//       $winRatio: {"$multiply": [ { "$divide": ["$wins","$losses"] }, 100 ]}
+//     }
+//   },
+//   {
+//     $sort: {winRatio: 1}
+//   }
+// ]);
