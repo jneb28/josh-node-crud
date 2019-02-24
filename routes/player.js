7d9adc3bb9237041e3cacb4fe9f8035882c42df8
Joshua Nebeker <10528790@my.uvu.edu>
2019-02-24 12:01:45 -0700

    fixed route name

--- routes/player.js
+++ routes/player.js
@@ -5,7 +5,7 @@ const PlayerController = require('../controllers/player.js');
 
 //router.get('/add-player', PlayerController.addPlayer);
 
-router.post('/add-player', PlayerController.postPlayer);
+router.post('/create-player', PlayerController.postPlayer);
 
 //router.get('/view-player/:playerId', PlayerController.viewPlayer);
 router.get('/view-player/:id', PlayerController.viewPlayer);
