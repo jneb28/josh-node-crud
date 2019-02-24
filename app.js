914ab5ccbce243b9311ce91b6d3f092783aa4cca
Joshua Nebeker <10528790@my.uvu.edu>
2019-02-24 12:14:37 -0700

    set header

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ app.set('views', 'views');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 //app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+})
 
 const playerRoutes = require('./routes/player.js');
 app.use(playerRoutes);
