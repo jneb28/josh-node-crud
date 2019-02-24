59da0175e0ed0b694595901bddaf97edff9ce814
Joshua Nebeker <10528790@my.uvu.edu>
2019-02-23 20:24:41 -0700

    form styling

--- app.js
+++ app.js
@@ -6,28 +6,18 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
-
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const ladderRoutes = require('./routes/ladder.js');
 const playerRoutes = require('./routes/player.js');
-app.use(ladderRoutes);
 app.use(playerRoutes);
 
-
-
-//mongoose.connect('mongodb+srv://jneb:likgvQyckdgq9psk@cluster0-qbjhd.mongodb.net/test?retryWrites=true')
 mongoose.connect(process.env.MONGODB_URI)
 .then(result => {
-  console.log('Connected!');
-  
-  app.listen(process.env.PORT || port);
-  
+  console.log('Connected to Cluster!');
+  app.listen(process.env.PORT || port);  
 })
-.catch(err => {
-  console.log(err);
-});
+.catch(err => console.log(err));
--- controllers/ladder.js
+++ controllers/ladder.js
@@ -1,16 +1,2 @@
-const Player = require('../models/player.js');
 
-exports.getLadder = (req, res, next) => {
-  Player.find()
-  .then(result => {
-    res.render('ladder/index.ejs', {
-      path: '/',
-      title: 'Ladder',
-      players: result
-    });
-  })
-  .catch(err => {
-    console.log(err);
-  });
-};
 
